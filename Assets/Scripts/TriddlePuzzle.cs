5c2e502fd98c36595c09098c5befd6989ab132ad
Hunter Dyar <bloop@hdyar.com>
2020-02-09 19:31:12 -0500

    creating the hints (the labels at edge) from solution data.
     Gotta figure out when to generate all this (shouldnt need to store it...) and how to display it in the right spot.

--- Assets/Scripts/TriddlePuzzle.cs
+++ Assets/Scripts/TriddlePuzzle.cs
@@ -12,6 +12,11 @@ namespace Blooper.Triangles{
     }
     
 [CreateAssetMenu(fileName = "puzzle",menuName = "Triddle/puzzle", order = 120)]
+    public struct HintItem{
+        public HintItem(int a,int c){q = a;color = c;}
+        public int q;
+        public int color;
+    }
     public class TriddlePuzzle : ScriptableObject
     {
         public TridSize tridSize;
@@ -22,5 +27,32 @@ namespace Blooper.Triangles{
         public int[][] b_solution;
         public int[][] bl_solution;
         public int[][] br_solution;
+
+        public HintItem[][] t_solution_hint;
+
+        public HintItem[] GetHintFromSolution(int[] solution){
+            //give a solution, return the appropriatley concatenated hint, as a string. So 1,1,0,0,0,3,3,3 becomes 2,2.
+            //For now, lets consider monochromatic hints. Otherwise some non-int form may be needed, like a bit shift trick or just a fucking string lol.
+            int prev = 0;
+            List<HintItem> hint = new List<HintItem>();
+            //make a list of hintItems with each color and Amount.
+            for(int i = 0;i<solution.Length;i++){
+                int j = solution[i];
+                if(j != prev){//
+                    hint.Add(new HintItem(1,j));//start with one.
+                }else{
+                    //all this does is increase q(uantity) by one.
+                    hint[hint.Count-1] = new HintItem(hint[hint.Count-1].q+1,hint[hint.Count-1].color);
+                }
+                prev = i;
+            }
+            //But get rid of the ones that aren't actually colors. (color, state, same thing basically.)
+            for(int i = hint.Count-1;i>0;i--){
+                if(hint[i].color == 0){
+                    hint.RemoveAt(i);
+                }
+            }
+            return hint.ToArray();
+        }
     }
 }
