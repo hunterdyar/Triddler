ae874942bd85e0d0e88feff2f0304c8b74744717
Hunter Dyar <bloop@hdyar.com>
2020-02-16 19:54:55 -0500

    new level loading system
    - hide and show in-game menu with m
    - upload or enter URL form from javascript form below webGL window
    - new WebGL template to get this done
    - some documentaiton
    - various built target/settings changed

---
+++ Assets/Scripts/CanvasHider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasHider : MonoBehaviour
+{
+    public GameObject toggle;
+    public bool hideOnStart;
+    void Start(){
+        toggle.SetActive(!hideOnStart);
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.M)){
+            toggle.SetActive(!toggle.activeInHierarchy);
+        }
+    }
+}
--- Assets/Scripts/LoadLevelFromWeb.cs
+++ Assets/Scripts/LoadLevelFromWeb.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
+using System.Runtime.InteropServices;
+
 namespace Blooper.Triangles{
     public class LoadLevelFromWeb : MonoBehaviour
     {
@@ -10,11 +12,27 @@ namespace Blooper.Triangles{
         public InputField addressInput;
         public TriddlePuzzle puzzle;
         public TriangleGridSystem triangleGridSystem;
+        void Start () {
+        //    TextUploaderInit ();
+        }
         // Update is called once per frame
         public void LoadLevelFromAddress()
         {
             address = addressInput.text;
             StartCoroutine(DoLoadFromAddress());
+        }
+        public void LoadLevelFromAddressOnClipboard()
+        {
+            string clipboard = GUIUtility.systemCopyBuffer;
+            if(!System.Uri.IsWellFormedUriString(clipboard,System.UriKind.Absolute)){
+                //invalid URL in clipboard, must be something else, try to load whatever was typed.
+                LoadLevelFromAddress();
+            }else{
+                //load from the clipboard.
+                address = clipboard;
+                addressInput.text = address;
+                StartCoroutine(DoLoadFromAddress());
+            }
         }
         IEnumerator DoLoadFromAddress()
         {
@@ -30,5 +48,30 @@ namespace Blooper.Triangles{
                 Debug.Log("loaded!");
             }
         }
-    }
+        ///
+
+        ///Loading from file picker
+
+        // [DllImport("__Internal")]
+        // private static extern void TextUploaderInit();
+    
+        IEnumerator LoadText (string address) {
+            UnityWebRequest www = UnityWebRequest.Get(address);
+            yield return www.SendWebRequest();
+            if(www.isNetworkError || www.isHttpError) {
+                Debug.Log(www.error);
+            } else
+            {
+                puzzle.levelAsTextData = www.downloadHandler.text;
+                triangleGridSystem.LoadLevel();
+                Debug.Log("loaded from File!");
+            }
+        }
+        //called by the TextUploder.jslib library
+        void FileSelected (string url) {
+            StartCoroutine(LoadText (url));
+        }
+    
+        
+        }
 }
